4936d04428232792d04b17ae29c4f3f887a4fbf3
Meegan1 <jake@meegan.me>
2026-02-02 16:14:39 +0000

    feat: add exrc option to allow local vimrc files

--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,7 @@
 require("config.options")
 require("config.yank")
 require("config.keymaps")
+require("config.exrc")
 
 require("lze").register_handlers(require("utils.lzeCats").for_cat)
 require("lze").register_handlers(require("utils.lzeCats").for_cat_value)
